dde4e434cc008b66c3e99d5c3a9ecb0eda2970c0
Mx. Kas Perch <the@nodebotani.st>
2020-07-09 09:26:43 -0500

    Section on structs complete

---
+++ Rust/structs/src/main.rs
@@ -0,0 +1,33 @@
+#[derive(Debug)]
+struct User{
+    name: String,
+    age: i32,
+    height: i32,
+    shoesize: i32,
+}
+
+impl User {
+    fn simple_string(&self) -> String{
+        format!("{} - {} - {}cm - shoe:{}", self.name, self.age, self.height, self.shoesize)
+    }
+
+    fn grow(&mut self, h:i32) {
+        self.height += h;
+    }
+
+    fn die(self) { // consumes the struct 
+        println!("DEAD: {}", self.simple_string());
+    }
+}
+
+fn main() {
+    let mut u = User {
+        name: "Matt".to_string(),
+        age: 33,
+        height: 250,
+        shoesize: 10,
+    };
+
+    u.grow(20);
+    println!("User is {}", u.simple_string());
+}
